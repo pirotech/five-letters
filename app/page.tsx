7f0cfa59a2a2f72ede188a87f26f97c1cd78ef13
Sergey <serjioini@gmail.com>
2023-11-14 18:12:27 +0300

    стили украшения

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Game } from '@/app/Game';
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-between p-12"
-      style={{ backgroundColor: '#fcfcfc' }}
+      className="flex min-h-screen flex-col items-center justify-between"
+      style={{ backgroundColor: '#fcfcfc', height: '100%' }}
     >
       <Game />
     </main>
